bfbe355e446a17edbfcd2a97145252d01e8bcdc3
LucasCoelho <lcsilva2099@gmail.com>
2026-06-29 17:37:24 -0300

    Report uncategorized characters in category context runner

--- fyi-category-context-run.ts
+++ fyi-category-context-run.ts
@@ -3,12 +3,19 @@ import { getDokkanFyiCategoryContext, writeDokkanFyiCategoryContext } from "./fy
 async function main() {
     const outputPath = await writeDokkanFyiCategoryContext();
     const dataset = await getDokkanFyiCategoryContext();
+    const uncategorizedCharacters = dataset.characters.filter(character => character.categoryIds.length === 0);
 
     console.log(JSON.stringify({
         outputPath,
         categoryCount: dataset.categoryCount,
         supportMemoryCount: dataset.supportMemoryCount,
         characterCount: dataset.characterCount,
+        uncategorizedCharacterCount: uncategorizedCharacters.length,
+        uncategorizedSample: uncategorizedCharacters.slice(0, 10).map(character => ({
+            id: character.id,
+            name: character.name,
+            title: character.title,
+        })),
         firstCharacter: dataset.characters[0] ? {
             id: dataset.characters[0].id,
             categories: dataset.characters[0].categoryIds.length,
